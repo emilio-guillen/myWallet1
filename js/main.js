17d7fbd0c0c15fb6d0ed4cb4af1f423da9d224a7
FittestLeek7751 <emilio0098@gmail.com>
2023-03-07 13:13:28 -0600

    terceraPreentrega

--- js/main.js
+++ js/main.js
@@ -1,83 +1,160 @@
-class Usuario {
-    constructor (nombre, email, contraseña, saldo) {
-        this.nombre = nombre
-        this.email = email
-        this.contraseña = contraseña
-        this.saldo = saldo
+// Constructor Destinatario
+class Destinatario {
+    constructor (nombre, numeroCuenta) {
+        this.nombre = nombre;
+        this.numeroCuenta = numeroCuenta;
+        this.saldo = 0;
     }
+}
+//Funcion Renderizar Destinatarios
+
+function renderizarDestinatarios (destinatarios) {
+    bodyTabla.innerHTML = "";
+
+    destinatarios.forEach( (destinatario) => {
+        const tr = document.createElement("tr");
+
+        const tdNombre = document.createElement("td");
+        const span = document.createElement("span");
+        span.innerHTML = `${destinatario.nombre}`,
+        tdNombre.append(span);
+
+        const tdCuenta = document.createElement("td");
+        tdCuenta.innerHTML = `${destinatario.numeroCuenta}`;
+
+        const tdSaldo = document.createElement("td");
+        tdSaldo.innerHTML = `$${destinatario.saldo}`;
+
+        tr.append(tdNombre);
+        tr.append(tdCuenta);
+        tr.append(tdSaldo);
+
+        bodyTabla.append(tr)
+    })
+}
+
+//Eventos agregar  y retirar saldo
+
+const bodyTabla = document.getElementById("bodyTabla")
+let saldoUsuario = parseInt(obtenerSaldo()) 
+const listaDeDestinatarios = obtenerDestinatarios();
+renderizarDestinatarios(listaDeDestinatarios);
+//console.log(listaDeDestinatarios);
+
+const formularioAgregarSaldo = document.getElementById("formularioAgregar");
+const inputSaldoAgregar = document.getElementById("montoAgregar");
+const formularioRetirarSaldo = document.getElementById("formularioRetirar");
+const inputMontoRetirar = document.getElementById ("montoRetirar");
+const formularioDestinatarios = document.getElementById("formularioDestinatarios");
+const inputNombreDestinatario = document.getElementById("nombreDestinatario");
+const inputNumeroCuenta = document.getElementById("cuentaDestinatario");
+const formularioTransferir = document.getElementById("formularioTransferir");
+const inputDestinatarioTransferir = document.getElementById("destinatarioTransferir");
+const inputMontoTransferir = document.getElementById("montoTransferir")
+
+
+formularioAgregarSaldo.addEventListener("submit", (event) => {
+
+    event.preventDefault();
+
+    saldoUsuario += parseInt(inputSaldoAgregar.value);
+    saldoActual.innerHTML = `Tu saldo actual es $${saldoUsuario}`;
+    inputSaldoAgregar.value = "";
+    actualizarLS();
     
-    añadirSaldo (cantidad) {
-        this.saldo += cantidad
-        alert(`El nuevo saldo de ${this.nombre} es ${this.saldo}`)
-    }
-    retirar (cantidad) {
-        this.saldo -= cantidad
-        alert(`Tu nuevo saldo es ${this.saldo}`)
-    }
-    transferir (cantidad,destinatario) {
-        this.retirar(cantidad);
-        destinatario.añadirSaldo(cantidad)
-    }
-
-}
-const usuario1 = new Usuario ("Emilio", "emilio@mail.com", "password123", 200)
-const usuario2 = new Usuario ("Romina", "romi@mail.com", "password123", 500)
-const usuario3 = new Usuario ("Mario", "mario@mail.com", "password123", 400)
-const usuario4 = new Usuario ("Fany", "fany@mail.com", "password123", 600)
-
-const usuarios = [usuario1, usuario2, usuario3, usuario4]
-
-function bienvenida (usuario){
-    alert(`Bienvenido ${usuario.nombre}, tu saldo es ${usuario.saldo}`)
-}
-function despedida (usuario){
-    alert(`Adiós ${usuario.nombre}`)
-}
-function menu (opcion) {
-    while (opcion != 4) {
-        switch(opcion){
-            case "1":
-            cantidad = parseFloat(prompt("¿Cuánto deseas añadir?:"))
-            usuario1.añadirSaldo(cantidad)
-            break;
-            case "2":
-            cantidad = parseFloat(prompt("¿Cuánto deseas retirar?:"))
-            usuario1.retirarSaldo(cantidad)
-            break;
-            case "3":
-            cantidad = parseFloat(prompt("¿Cuánto deseas transferir?:"))
-            destinatario = escogerDestinatario()
-            usuario1.transferir(cantidad, destinatario)
-            break;
-            default:
-            alert("Opción inválida")
-            break;
-        }
-        opcion=prompt("¿Qué deseas hacer? 1. Añadir saldo 2. Retirar 3. Transferir 4. Salir")
-    }
-}
-
-bienvenida(usuario1)
-usuarios.forEach((usuario)=>{
-    console.log(usuario.nombre)
 })
-let opcion=prompt("¿Qué deseas hacer? 1. Añadir saldo 2. Retirar 3. Transferir 4. Salir")
-menu(opcion)
-despedida(usuario1)
+
+formularioRetirarSaldo.addEventListener("submit", (event) => {
+
+    event.preventDefault();
+
+    saldoUsuario -= parseInt(inputMontoRetirar.value);
+    saldoActual.innerHTML = `Tu saldo actual es $${saldoUsuario}`;
+    inputMontoRetirar.value = "";
+    actualizarLS();
+    
+})
+
+formularioDestinatarios.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const nombre = inputNombreDestinatario.value;
+    const numeroCuenta = inputNumeroCuenta.value;
+
+    inputNombreDestinatario.value = "";
+    inputNumeroCuenta.value = "",
+
+    listaDeDestinatarios.push(new Destinatario(nombre, numeroCuenta));
+    //console.log(listaDeDestinatarios)
+    renderizarDestinatarios(listaDeDestinatarios);
+
+    actualizarDestinatariosLS();
+})
+
+formularioTransferir.addEventListener("submit", (event)=> {
+    event.preventDefault();
+
+    saldoUsuario -= parseInt(inputMontoTransferir.value);
+    saldoActual.innerHTML = `Tu saldo actual es $${saldoUsuario}`;
+    const nombrePersona = inputDestinatarioTransferir.value;
+    const cantidadTransferir = inputMontoTransferir.value;
+    
+    const indicePersona = listaDeDestinatarios.findIndex((persona) => {
+        return persona.nombre === nombrePersona
+    })
+
+    listaDeDestinatarios[indicePersona].saldo += parseInt(cantidadTransferir);
+    
+    
+    inputDestinatarioTransferir.value = "";
+    inputMontoTransferir.value = "",
+
+    renderizarDestinatarios(listaDeDestinatarios);
+    actualizarDestinatariosLS();
+    actualizarLS();
+})
+
+inputDestinatarioTransferir.addEventListener("input", () => {
+    const buscarDestinatario = inputDestinatarioTransferir.value;
+
+    const destinatariosFiltrados = listaDeDestinatarios.filter ((destinatario) => {
+        return destinatario.nombre.toLowerCase().includes(buscarDestinatario.toLowerCase());
+    });
+
+    renderizarDestinatarios(destinatariosFiltrados);
+})
 
 
-function escogerDestinatario() {
-    destinatario = prompt("Escribe el nombre del destinatario (Lista en la consola)")
-    switch(destinatario){
-        case "Romina":
-        return usuario2;
-        case "Mario":
-        return usuario3;
-        case "Fany":
-        return usuario4;
-        default:
-        alert("Opción inválida")
-        break;
+function obtenerSaldo () {
+    const saldoActual = document.getElementById("saldoActual");
+    let saldoInicial = 0;
+
+    let saldoLS = localStorage.getItem("saldoActualKey");
+
+    if (saldoLS !== null) {
+        saldoInicial = parseInt(saldoLS);
+        saldoActual.innerHTML = `Tu saldo actual es $${saldoInicial}`;
     }
+    return saldoInicial;
 }
 
+function obtenerDestinatarios () {
+    let destinatarios = [];
+
+    let destinatariosLS = localStorage.getItem("destinatariosKey")
+
+    if (destinatariosLS !== null) {
+        destinatarios = JSON.parse(destinatariosLS)
+    }
+    return destinatarios
+}
+
+function actualizarLS () {
+    localStorage.setItem("saldoActualKey", saldoUsuario);
+}
+
+function actualizarDestinatariosLS() {
+    const listaDestinatariosJSON = JSON.stringify(listaDeDestinatarios);
+    localStorage.setItem("destinatariosKey", listaDestinatariosJSON)
+
+}
